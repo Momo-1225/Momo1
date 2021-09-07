46e85a6428fa3d217ca65e289301052cd6173bd7
Momo-1225 <90081502+Momo-1225@users.noreply.github.com>
2021-09-06 22:47:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ var mywindow = window.open("", "_self")
 
 function School() {
   mywindow.open("https://www.utoronto.ca");
-};
+}
+
+function FB() {
+  mywindow.open("");
+}
+
+function Insta() {
+  mywindow.open("");
+}
 
 function Linkedin() {
   mywindow.open("https://www.linkedin.com/in/mohamed-mohamed-0ab9b7210/");
