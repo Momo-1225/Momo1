f05501c91c59a7b81a5c3754ff9fe9a5ef69f000
Momo-1225 <90081502+Momo-1225@users.noreply.github.com>
2021-09-06 22:41:16 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var mywindow = window.open("", "_self")
+
+function School() {
+  mywindow.open("https://www.utoronto.ca");
+} 
+
+function Linkedin() {
+  mywindow.open("https://www.linkedin.com/in/mohamed-mohamed-0ab9b7210/");
+}
